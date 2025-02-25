ec351b3d175ac601f33f50f4549e4f99edd4dc16
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-25 14:55:01 -0300

    created: archivo 1 y 2 de javascript

---
+++ 1_funcion.js
@@ -0,0 +1,12 @@
+/*
+ function example(a, b, c){
+ return a+b+c
+ }
+*/
+
+//función de expresión
+const sumar = function(a,b,c){
+	return a + b + c;
+}
+
+console.log(sumar(1,1,1));
---
+++ 2_arrow.js
@@ -0,0 +1,10 @@
+/*
+ let suma = function(a, b){
+ return a + b
+ }
+*/
+
+//Arrow function
+const suma = (a,b) => a + b;
+
+console.log(suma(1,2))
