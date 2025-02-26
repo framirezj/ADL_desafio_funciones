b5e5a8a2b1d9f4d5976d4ac9cfa67f1f1794863a
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-26 20:47:07 -0300

    created: pintar el div key con teclas ASD

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,14 +27,5 @@ const divYellow = document.querySelector('#divYellow');
     divYellow.addEventListener('click', () => colorBlack(divYellow))
 
 
-/* Crea una página junto a un script que guarde dentro de una variable global
-un color dependiendo de la letra del teclado presionada. (2 Puntos).
-○ La letra a guardará el color rosado.
-○ La letra s guardará el color naranjo.
-○ La letra d guardará el color celeste.
-○ Para guardar el color revisa el tip al final del enunciado.
-○ Crea un nuevo div con el id “key”, de 200 px de ancho y alto y de
-color blanco y borde negro.
-○ Al presionar las teclas a, s o d, se deberá cambiar el color del div
-“key” a rosado, naranjo o celeste respectivamente */
+
 
