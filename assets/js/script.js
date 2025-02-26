ac3eb737d8a8e8ccb0d4a4c5947b5a4e75759902
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-26 19:36:38 -0300

    created: funcionalidad punto 2 en 4_colores.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,30 @@ const ele = document.getElementById("ele1")
 divs, este cambie de color al color negro. Utiliza addEventListener para
 agregar el evento. (1 Punto) */
 
+
+const colorBlack = (elemento) => elemento.style.backgroundColor = "black"; 
+
 const divBlue = document.querySelector('#divBlue');
-    divBlue.addEventListener("click", () => console.log("hola"))
+    divBlue.addEventListener("click", () => colorBlack(divBlue))
 
 const divRed = document.querySelector('#divRed');
-    divRed.addEventListener('click', () => console.log("holaa"))
+    divRed.addEventListener('click', () => colorBlack(divRed))
 
 const divGreen = document.querySelector('#divGreen');
-    divGreen.addEventListener('click', () => console.log("holaa"))
+    divGreen.addEventListener('click', () => colorBlack(divGreen))
 
 const divYellow = document.querySelector('#divYellow');
-    divYellow.addEventListener('click', () => console.log("holaa"))
+    divYellow.addEventListener('click', () => colorBlack(divYellow))
+
+
+/* Crea una página junto a un script que guarde dentro de una variable global
+un color dependiendo de la letra del teclado presionada. (2 Puntos).
+○ La letra a guardará el color rosado.
+○ La letra s guardará el color naranjo.
+○ La letra d guardará el color celeste.
+○ Para guardar el color revisa el tip al final del enunciado.
+○ Crea un nuevo div con el id “key”, de 200 px de ancho y alto y de
+color blanco y borde negro.
+○ Al presionar las teclas a, s o d, se deberá cambiar el color del div
+“key” a rosado, naranjo o celeste respectivamente */
+
