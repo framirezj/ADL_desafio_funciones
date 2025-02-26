73c2eac65ab28f881bcb6221daf3735d31d0dc1d
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-26 19:25:22 -0300

    created: pagina 4colores.html con los 2 puntos realizados

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,21 @@ function pintar(elemento, color = 'green'){
     }
 
 const ele = document.getElementById("ele1")
-    ele.addEventListener("click", () => pintar(ele, 'yellow'));
+    ele.addEventListener("click", () => pintar(ele, 'yellow') );
 
 
+/* Dentro del script agrega el evento para que al hacer click a cada uno de los
+divs, este cambie de color al color negro. Utiliza addEventListener para
+agregar el evento. (1 Punto) */
 
-    
+const divBlue = document.querySelector('#divBlue');
+    divBlue.addEventListener("click", () => console.log("hola"))
+
+const divRed = document.querySelector('#divRed');
+    divRed.addEventListener('click', () => console.log("holaa"))
+
+const divGreen = document.querySelector('#divGreen');
+    divGreen.addEventListener('click', () => console.log("holaa"))
+
+const divYellow = document.querySelector('#divYellow');
+    divYellow.addEventListener('click', () => console.log("holaa"))
